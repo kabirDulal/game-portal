4108db93413311daaad56e73a78bd5e2f1b387e4
Kabir Dulal <kabird@stridesolutions.com.au>
2025-06-04 09:39:47 +1000

    creating props  and sending data and functions to the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import ListGroup from "./components/ListGroup";
 function App() {
+   let cities = ["New York", "Tokyo", "Kathmandu", "Sydney", "Melbourne"];
+  const  handleSelectItem = (item:string)=>{
+    console.log({item})
+  }
   return (
     <div>
-      <ListGroup />
+      <ListGroup  items = {cities} heading = 'List of Cities' onSelectItem={handleSelectItem}/>
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,26 +1,29 @@
 import {useState} from "react";
-
-function ListGroup() {
-  let cities = ["New York", "Tokyo", "Kathmandu", "Sydney", "Melbourne"];
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string)=> void;
+}
+function ListGroup({items, heading, onSelectItem}: Props) { 
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List Items</h1>
-      {cities.length === 0 ? (
+      <h1>{heading}</h1>
+      {items.length === 0 ? (
         <p>No Cities Found</p>
       ) : (
         <ul className="list-group">
-          {cities.map((item, index) => (
+          {items.map((item, index) => (
             <li
               className={
                 selectedIndex === index
                   ? "list-group-item active"
                   : "list-group-item"
-              }
-              key={item}
-              onClick={() => {
+              }            
+              key= {item} onClick={() => {
                 setSelectedIndex(index);
+                onSelectItem(item);
               }}>
               {item}
             </li>
