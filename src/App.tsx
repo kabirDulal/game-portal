fff9034da3a25780cb999535f5e4a16009d89a2d
Kabir Dulal <kabird@stridesolutions.com.au>
2025-06-04 12:56:34 +1000

    made changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 function App() {
-   let cities = ["New York", "Tokyo", "Kathmandu", "Sydney", "Melbourne"];
-  const  handleSelectItem = (item:string)=>{
-    console.log({item})
+  let cities = ["New York", "Tokyo", "Kathmandu", "Sydney", "Melbourne"];
+  const handleSelectItem = (item: string) => {
+    console.log({ item })
   }
   return (
-    <div>
-      <ListGroup  items = {cities} heading = 'List of Cities' onSelectItem={handleSelectItem}/>
-    </div>
+    <>
+      <div className="mb-3">
+        <ListGroup items={cities} heading='List of Cities' onSelectItem={handleSelectItem} />
+      </div>
+      <div className="alert alert-primary">
+        <Alert>
+          Hello <span>World </span>  
+        </Alert>
+      </div>
+      <div type = "button" className="btn btn-secondary">
+        <Button />
+      </div>
+    </>
   );
 }
 export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+interface Props {
+	children: string;
+}
+
+const Button = () => {
+  return (
+    <div type = "button" className="btn btn-secondary">
+	Click Me
+    </div>
+  )
+}
+
+export default Button
